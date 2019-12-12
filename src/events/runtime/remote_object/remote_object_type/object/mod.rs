c7d88a4c5b146093e4a280919eab8b1ca0b3a5b6
Avery Harnish <averyharnish@gmail.com>
2019-12-12 15:58:38 -0600

    Preview ObjectPreview Entries

--- src/events/runtime/remote_object/remote_object_type/object/mod.rs
+++ src/events/runtime/remote_object/remote_object_type/object/mod.rs
@@ -1,41 +1,108 @@
-mod property;
-mod subtype;
+pub mod entry;
+pub mod property;
+pub mod subtype;
 
 use std::fmt;
 
+pub use entry::ObjectEntry;
 pub use property::ObjectProperty;
 pub use subtype::ObjectSubtype;
+
+use super::RemoteObjectType;
 
 #[derive(Debug, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct ObjectPreview {
     #[serde(rename = "type")]
-    pub object_type: String,
+    pub object_type: RemoteObjectType,
     pub description: String,
     pub overflow: bool,
-    pub subtype: Option<String>,
+    pub subtype: Option<ObjectSubtype>,
     pub properties: Vec<ObjectProperty>,
+    pub entries: Option<Vec<ObjectEntry>>,
+}
+
+impl ObjectPreview {
+    fn parse_error(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        log::debug!("{:?}", &self);
+        write!(f, "{{unknown ")?;
+        if let Some(subtype) = &self.subtype {
+            write!(f, "{}", subtype)?;
+        } else {
+            write!(f, "{}", &self.object_type)?;
+        }
+        write!(f, "}}")
+    }
+
+    fn write_overflow(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        if self.overflow {
+            write!(f, ", …")
+        } else {
+            Ok(())
+        }
+    }
 }
 
 impl fmt::Display for ObjectPreview {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let len = self.properties.len();
-        if len > 0 {
-            let last_index = len - 1;
-            for (idx, property) in &mut self.properties.iter().enumerate() {
-                if idx == 0 {
-                    write!(f, "{{")?;
+        if let Some(subtype) = &self.subtype {
+            match subtype {
+                ObjectSubtype::Array => {
+                    write!(f, " [")?;
+                    let last_index = self.properties.len() - 1;
+                    for (index, property) in &mut self.properties.iter().enumerate() {
+                        if property.object_type.as_str() == "string" {
+                            if let Some(value) = &property.value {
+                                write!(f, "\"{}\"", value)?;
+                            } else {
+                                self.parse_error(f)?;
+                            }
+                        } else {
+                            write!(f, "{}", property)?;
+                        }
+                        if index < last_index {
+                            write!(f, ", ")?;
+                        }
+                    }
+                    self.write_overflow(f)?;
+                    write!(f, "]")
                 }
-                write!(f, "{}", property)?;
-                if idx < last_index {
-                    write!(f, ", ")?;
-                } else {
-                    write!(f, "}}")?;
+                ObjectSubtype::Map => {
+                    if let Some(entries) = &self.entries {
+                        write!(f, "{{")?;
+                        let last_index = entries.len() - 1;
+                        for (index, entry) in &mut entries.iter().enumerate() {
+                            write!(f, "{}", entry)?;
+                            if index < last_index {
+                                write!(f, ", ")?;
+                            }
+                        }
+                        self.write_overflow(f)?;
+                        write!(f, "}}")
+                    } else {
+                        self.parse_error(f)
+                    }
                 }
+                ObjectSubtype::RegExp | ObjectSubtype::Date => write!(f, "{}", &self.description),
+                _ => write!(f, "{}", subtype),
             }
         } else {
-            write!(f, "{{}}")?;
+            match &self.object_type {
+                RemoteObjectType::Object => {
+                    write!(f, "{{")?;
+                    let last_index = self.properties.len() - 1;
+                    for (idx, property) in &mut self.properties.iter().enumerate() {
+                        write!(f, "{}", property)?;
+                        if idx < last_index {
+                            write!(f, ", ")?;
+                        }
+                    }
+                    self.write_overflow(f)?;
+                    write!(f, "}}")
+                }
+                RemoteObjectType::String => write!(f, "\"{}\"", &self.description),
+                _ => write!(f, "{}", &self.description),
+            }
         }
-        Ok(())
     }
 }
