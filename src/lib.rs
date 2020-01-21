5f8dd641cb586f5605a1aeaec8bd5b598c459ac0
Avery Harnish <averyharnish@gmail.com>
2020-01-21 12:58:28 -0600

    wip tests

--- src/lib.rs
+++ src/lib.rs
@@ -7,3 +7,49 @@
 pub mod domain;
 
 pub use domain::{runtime, runtime::Runtime, Domain};
+
+#[cfg(test)]
+mod tests {
+    use crate::Domain;
+    #[test]
+    fn it_can_deserialize_bigint_log() {
+        let input = r#"{{
+    "method": "Runtime.consoleAPICalled",
+    "params": {{
+      "type": "log",
+      "args": [
+        {{
+          "type": "bigint",
+          "unserializableValue": "1000n",
+          "description": "1000n"
+        }}
+      ],
+      "executionContextId": 2,
+      "timestamp": 1576105132037,
+      "stackTrace": {{
+        "callFrames": [
+          {{
+            "functionName": "handleRequest",
+            "scriptId": "3",
+            "url": "worker.js",
+            "lineNumber": 17,
+            "columnNumber": 10
+          }},
+          {{
+            "functionName": "",
+            "scriptId": "3",
+            "url": "worker.js",
+            "lineNumber": 1,
+            "columnNumber": 20
+          }}
+        ]
+      }}
+    }}
+  }}"#;
+        let output: Result<Domain, _> = serde_json::from_str(&input);
+        if let Err(e) = output {
+            println!("{}", e);
+        }
+        // assert!(output.is_ok())
+    }
+}
