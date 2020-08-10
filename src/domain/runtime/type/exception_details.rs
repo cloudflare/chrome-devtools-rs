08219de627424c101e64dc164e2a96201cd18f30
Joshua Johnson <jspspike@gmail.com>
2020-08-10 12:39:01 -0500

    Revert "Added line and column number to ExceptionDetails"
    This reverts commit a358d6a181354a533ee16ecf8a1d5d4cd2a306ba.

--- src/domain/runtime/type/exception_details.rs
+++ src/domain/runtime/type/exception_details.rs
@@ -3,12 +3,9 @@ use serde::{Deserialize, Serialize};
 /// Detailed information about exception (or error) that was thrown during script compilation or execution.
 /// See [ExceptionDetails](https://chromedevtools.github.io/devtools-protocol/tot/Runtime#type-ExceptionDetails)
 #[derive(Debug, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
 pub struct ExceptionDetails {
     pub text: String,
     pub exception: Exception,
-    pub line_number: usize,
-    pub column_number: usize,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
